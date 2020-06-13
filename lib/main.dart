9f3220d7705a48987984080986cf54413a244e27
divyesh <skdivyesh333@gmail.com>
2020-06-13 18:20:57 +0530

    changed to stateful widget added random number generation when button is pressed both dice are chnaged when 1 of them is clicked

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,7 +16,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = Random().nextInt(6) + 1;
+  int rightDiceNumber = Random().nextInt(6) + 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,16 +31,26 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
               onPressed: () {
-                print('Left button pressed');
+                setState(
+                  () {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  },
+                );
+                print('left button pressed');
               },
             ),
           ),
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice5.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
               onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
                 print('Right button pressed');
               },
             ),
