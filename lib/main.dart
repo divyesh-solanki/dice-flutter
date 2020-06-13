0069b8844f19700cc8ba4e56e768c11da4bd61dd
divyesh <skdivyesh333@gmail.com>
2020-06-13 18:52:10 +0530

    final commit made a function for changing dice face

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-
 void main() {
   return runApp(
     MaterialApp(
@@ -16,6 +15,8 @@ void main() {
   );
 }
 
+
+
 class DicePage extends StatefulWidget {
   @override
   _DicePageState createState() => _DicePageState();
@@ -24,6 +25,15 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = Random().nextInt(6) + 1;
   int rightDiceNumber = Random().nextInt(6) + 1;
+
+  void changeDice() {
+  setState(
+    () {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    },
+  );
+}
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -33,12 +43,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               child: Image.asset('images/dice$leftDiceNumber.png'),
               onPressed: () {
-                setState(
-                  () {
-                    leftDiceNumber = Random().nextInt(6) + 1;
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                  },
-                );
+                changeDice();
                 print('left button pressed');
               },
             ),
@@ -47,10 +52,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               child: Image.asset('images/dice$rightDiceNumber.png'),
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                });
+                changeDice();
                 print('Right button pressed');
               },
             ),
